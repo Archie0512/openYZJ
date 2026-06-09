1636a8f22ebd270bf6bb3213987bf9e3278100ec
Archie0512 <188044646@qq.com>
2026-06-09 12:02:08 +0800

    Revert "test: 临时测试移除卡片content字段 - 验证云之家是否只显示卡片无文本气泡"
    This reverts commit 6ccb0a3d94b94b261665ac02eefb2dc90b7ab15f.

--- server/app/services/handlers/mys4s_handler.py
+++ server/app/services/handlers/mys4s_handler.py
@@ -116,6 +116,7 @@ class MYS4SHandler(BaseHandler):
 
                             return YunzhijiaResponseData(
                                 type=25,
+                                content="",
                                 forwardControl="1",
                                 param=CardParam(
                                     baseInfo=CardBaseInfo(
